b887a88b977ed66a74f2283cd4c89e583ae9fbde
Charles Armitage <code@cjrarmitage.co.uk>
2017-06-24 22:58:52 +0100

    Initial commit

---
+++ msm-spike/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="msm_spike.MvcApplication" Language="C#" %>
---
+++ msm-spike/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(msm_spike.Startup))]
+namespace msm_spike
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
